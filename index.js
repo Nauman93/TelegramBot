4b060a028e6f5ae26a74f64ce977a3897c119920
Nauman93 <nauman@hotmail.it>
2018-04-09 19:20:15 +0200

    Working for photo only

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const token = '555213521:AAHsCKoF_qmgbbRnvj2JAb8c7glBkv39XY4';
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
-    
-    
-     
+var expression = /https?:\/\/?9gag\.com\/gag\/([a-z]?[A-Z]?[0-9]?)*/gi;
+
+bot.onText(expression, function(msg, match) {
+
+//bot.sendMessage(msg.chat.id, "aa");
+bot.sendPhoto(msg.chat.id, match[0]);
+
 });
