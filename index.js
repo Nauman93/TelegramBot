8f56eb5c824a8b9b74571fd1c48797687b870991
Nauman93 <nauman@hotmail.it>
2018-04-10 17:33:25 +0200

    index.js working when sharing images

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const token = '555213521:AAHsCKoF_qmgbbRnvj2JAb8c7glBkv39XY4';
 
+var gag = require('node-9gag');
+
 const bot = new TelegramBot(token, {polling: true});
 
+//regular expression for 9gag website with id
 var expression = /https?:\/\/?9gag\.com\/gag\/([a-z]?[A-Z]?[0-9]?)*/gi;
+var id = 
+
 
 bot.onText(expression, function(msg, match) {
 
 //bot.sendMessage(msg.chat.id, "aa");
-bot.sendPhoto(msg.chat.id, match[0]);
+gag.getItem(match[0], function (err, res) {
+  // res = {
+  //   title: ,
+  //   points: ,
+  //   commentCount: ,
+  //   image:
+  // }
+  //console.log(res);
+  //bot.sendPhoto(msg.chat.id, res.image);
+  bot.sendVideo(msg.chat.id, res.image);
+});
+
+
+
 
 });
