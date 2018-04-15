bfeb0672818ace0a597a4c9c21b3aa52458e1b38
Nauman93 <nauman@hotmail.it>
2018-04-15 18:32:54 +0200

    Add ability to save image/video locally from post

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 
 const TelegramBot = require('node-telegram-bot-api');
+    var fs = require('fs');
+	var request = require('request');
 
 const token = '555213521:AAHsCKoF_qmgbbRnvj2JAb8c7glBkv39XY4';
 
 var gag = {};
 
-gag.find = require('./lib/find');
-gag.getItem = require('./lib/getitem');
-gag.section = require('./lib/section');
+gag.getTopComment = require('./lib/gettopcomments');
+gag.getPost = require('./lib/getpost');
+
+
 
 const bot = new TelegramBot(token, {polling: true});
 
 //regular expression for 9gag website with id
 var expression = /https?:\/\/?9gag\.com\/gag\/([a-z]?[A-Z]?[0-9]?)*/gi;
 
+
+    //download function that make a request for the image or video
+    var download = function(uri, filename, callback){
+          request.head(uri, function(err, res, body){
+            //console.log('content-type:', res.headers['content-type']);
+            //console.log('content-length:', res.headers['content-length']);
+
+            request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+          });
+        };
 
 bot.onText(expression, function(msg, match) {
 
@@ -26,14 +39,47 @@ gag.getItem(postId, function (err, res) {
   //   commentCount: ,
   //   image:
   // }  
-  console.log(res);
+  //console.log(res);
   
-  if (res.type==='image'){
-  	bot.sendPhoto(msg.chat.id, res.image);
-  }
-  else if (res.type==='video'){
-  	bot.sendVideo(msg.chat.id, res.video);
-  }
+	if (res.type==='image'){
+		bot.sendPhoto(msg.chat.id, res.image); 
+
+		
+
+		bot.sendMessage(msg.chat.id, "Do you want me to save this?",{
+			"reply_markup": {
+				"keyboard": [["Yes", "No"]]
+    		}
+  		});
+
+  		bot.on('message', (msg) => {
+		var yesRes = "Yes";
+		if (msg.text.indexOf(yesRes) === 0) {
+    		download(res.image, res.title+'.jpg', function(){
+  			//console.log('done');
+			});;
+		}
+	});
+
+  	} else if (res.type==='video'){
+  		bot.sendVideo(msg.chat.id, res.video);
+
+  		bot.sendMessage(msg.chat.id, "Do you want me to save this?",{
+			"reply_markup": {
+				"keyboard": [["Yes", "No"]]
+    		}
+  		});
+
+  		bot.on('message', (msg) => {
+		var yesRes = "Yes";
+		if (msg.text.indexOf(yesRes) === 0) {
+    		download(res.video, res.title+'.mp4', function(){
+  				//console.log('done');
+			});;
+		}
+	});
+  	}
   
 });
 });
+
